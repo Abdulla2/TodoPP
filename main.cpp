4ccce133c62058b75c4fe182dbe333fe876cb0e6
Abdulla2 <abdullaseif2000@gmail.com>
2025-08-07 02:16:20 +0300

    Made the Todo manager and the basic functionality of it; Nothing optimized or benchmarked yet

--- main.cpp
+++ main.cpp
@@ -5,9 +5,12 @@
 #include <iostream>
 #include <istream>
 #include <iterator>
+#include <list>
+#include <memory>
 #include <ostream>
 #include <string>
 #include <string_view>
+#include <unordered_map>
 #include <utility>
 #include <vector>
 #include <sstream>
@@ -35,7 +38,7 @@ std::ostream& operator<<(std::ostream& out, const std::optional<T>& data)
 {
 	if (data) 
 	{
-	
+
 		return out << data.value();
 	}
 	return out;
@@ -113,7 +116,9 @@ public:
 
 	friend std::ostream& operator<<(std::ostream& out, const Date& date)
 	{
-		out << date.m_year << '-' << date.m_month << '-' << date.m_day;
+		out <<std::setfill('0') << std::setw(2) << date.m_year << '-' ;
+		out <<std::setfill('0') << std::setw(2) << date.m_month << '-';
+		out <<std::setfill('0') << std::setw(2) << date.m_day;
 		return out;
 	}
 
@@ -138,8 +143,7 @@ public:
 
 class Task
 {
-	// private:
-public:
+private:
 	// For now we'll define date as strings later we'll see what the STL offers for them
 	// using Date = std::string;
 	// Same goes here until finding about sets and maps in the STL
@@ -149,14 +153,15 @@ public:
 
 	bool m_isCompleted{};
 	Priority m_priority{};
-	Date m_completionDate {};
-	Date m_creationDate {};
+	std::optional<Date> m_completionDate {};
+	std::optional<Date> m_creationDate {};
 	std::string m_desc {};
 	std::vector<std::string> m_projectTags{};
 	std::vector<std::string> m_contextTags{};
 	std::vector<Set> m_specialTags{};
+	int m_id;
 
-	std::string full_text{};
+	// std::string full_text{};
 
 public:
 
@@ -168,7 +173,7 @@ public:
 		while ((pos = str.find(delim)) != std::string_view::npos) {
 			if(pos != 0)
 				vec.push_back(str.substr(0, pos));    // part before delimiter
-			
+
 			str.remove_prefix(pos + 1);           // move past delimiter
 		}
 
@@ -181,9 +186,9 @@ public:
 
 
 	Task() = default;
-	Task(std::string_view task)
+	Task(std::string_view task, int id = 0): m_id{id}
 	{
-		full_text = std::string{task};
+		// full_text = std::string{task};
 		// if(std::islower(task.front()))
 		auto vec  {split_by(task, ' ')};
 		auto words_n {vec.size()};
@@ -197,7 +202,7 @@ public:
 			m_isCompleted = true;
 		}
 		else
-		{
+	{
 			m_isCompleted = false;
 		}
 
@@ -208,7 +213,7 @@ public:
 			counter++;
 		}
 		else
-		{
+	{
 			m_priority = std::nullopt;
 		}
 
@@ -227,12 +232,22 @@ public:
 					m_creationDate = {vec[counter].substr(0,10)};
 					counter++;
 				}
+				else
+			{
+					m_creationDate = std::nullopt;
+				}
 			}
 			else
-			{
+		{
 				m_creationDate = {vec[counter].substr(0,10)} ;
+				m_completionDate = std::nullopt;
 				counter++;
 			}
+		}
+		else 
+	{
+			m_completionDate = std::nullopt;
+			m_creationDate = std::nullopt;
 		}
 
 		// m_desc = std::string{task};
@@ -261,10 +276,46 @@ public:
 
 	}
 
+	void setId(int id)
+	{
+		m_id = id;
+	}
+
+	const int getId() const { return m_id;}
+	const bool isCompleted() const { return m_isCompleted;}
+	void setCompleted() { m_isCompleted = true;}
+	void clearCompleted() { m_isCompleted = false;}
+
+	const std::vector<std::string> getProjects() const { return m_projectTags;} 
+	const std::vector<std::string> getContexts() const { return m_contextTags;} 
+	const std::vector<std::string> getSpecialTags() const { return m_specialTags;} 
+	const std::string_view getDesc() const { return m_desc;}
+
+	void addProject(const std::string_view project);
+	void addContext(const std::string_view context);
+	void addTag(const std::string_view tag);
+
+	void removeProject(const std::string_view project);
+	void removeContext(const std::string_view context);
+	void removeTag(const std::string_view tag);
+
+	const std::optional<char> getPriority() const { return m_priority;}
+	const std::optional<Date> getCreationDate() const { return m_creationDate;}
+	const std::optional<Date> getCompletionDate() const { return m_completionDate;}
+
+	void setPriority(char priority) { m_priority = priority;}
+	void clearPriority() { m_priority = std::nullopt;}
+
+	void setCompletionDate(Date& date);
+	void setCreationDate(Date& date);
+	void setCompletionDate(std::string_view);
+	void setCreationDate(std::string_view );
+
+
 	bool operator>(const Task& task) const
 	{
 		if (m_isCompleted != task.m_isCompleted) {
-		
+
 			return task.m_isCompleted;
 		}
 
@@ -276,7 +327,7 @@ public:
 			}
 
 			return m_priority < task.m_priority; // First chars are Higher priority
-		
+
 		}
 		else if (task.m_priority)
 		{
@@ -298,8 +349,23 @@ public:
 
 	friend std::ostream& operator<<(std::ostream& out, const Task& task)
 	{
-		out<<task.full_text<<'\n';
-		out << task.m_isCompleted <<std::setfill('0') << std::setw(2) << " (" << task.m_priority << ") " << task.m_completionDate << " " << task.m_creationDate << " " << task.m_desc << '\n';
+		// out<<task.full_text<<'\n';
+		if(task.m_isCompleted) out << "x ";
+		// out << (task.m_isCompleted ? "x " : "");
+
+		if (task.m_priority)
+		{
+			out << "(" << task.m_priority << ") ";
+		}
+		if (task.m_completionDate)
+		{
+			out << task.m_completionDate << " "; 
+		}
+		if(task.m_creationDate)
+		{
+			out << task.m_creationDate << " "; 
+		}
+		out << task.m_desc << '\n';
 		// out << "Context Tags: " << task.m_contextTags;
 		// out << "Project Tags: " << task.m_projectTags;
 		// out << "Special Tags: " << task.m_specialTags;
@@ -309,6 +375,7 @@ public:
 
 std::ostream& operator<<(std::ostream& out, const std::vector<Task>& vec)
 {
+
 	for(Task str: vec)
 	{
 		out<<str<<'\n';
@@ -317,6 +384,141 @@ std::ostream& operator<<(std::ostream& out, const std::vector<Task>& vec)
 	return out<<'\n';
 }
 
+class Todo
+{
+
+
+	std::vector<Task> m_tasks{};
+	// std::unordered_map<std::string, std::shared_ptr<Task>> projectsToTasks{};
+	bool m_fileDirty{false};
+	std::fstream m_file;
+	
+	const std::vector<Task> getTasksBy(const std::function<bool(const Task&)> searcher) const
+	// template <typename T>
+	// const std::vector<Task> getTasksBy(T searcher) const
+	{
+		std::vector<Task> vec{};
+		vec.reserve(3);
+
+
+		for(auto found {std::find_if(m_tasks.begin(), m_tasks.end(), searcher)}; found != m_tasks.end(); found = std::find_if(++found, m_tasks.end(), searcher))
+		{
+			vec.push_back(*found);
+		}
+
+		return vec;
+	}
+public:
+	Todo(std::fstream file, int size_of_file = 800)
+	{
+		// Average line length of 80
+		m_tasks.reserve(size_of_file/80);
+		int curr_line {0};
+		int curr_pos {0};
+		
+		for(Task curr_task{}; file>>curr_task;curr_line++)
+		{
+
+			// auto curr_task_ptr = m_tasks.emplace_back(std::make_shared<Task>(curr_task));
+			m_tasks.push_back(curr_task);
+			curr_task.setId(curr_line);
+			curr_pos = file.tellg();
+
+			// for(auto project: curr_task.getProjects())
+			// {
+			// 	projectsToTasks.insert_or_assign(project, curr_task_ptr);
+			//
+			// }
+
+			// temp_file_tasks.push_back(curr_task);
+			// lines_pos.push_back(std::pair<int,int>{i,file.tellg()});
+		}
+
+		bool file_dirty {false};
+		std::sort(std::execution::seq, m_tasks.begin(), m_tasks.end(), [&file_dirty](const Task& task1, const Task& task2)
+	    {
+	    // return task1.m_isCompleted > task2.m_isCompleted;
+	    if ( (task1 > task2) != (task1.getId() < task2.getId())) file_dirty = true;
+	    return task1 > task2;
+	    });
+		m_fileDirty = file_dirty;
+	}
+
+	void add(Task& task)
+	{
+		m_fileDirty = true;
+		m_tasks.push_back(task);
+	}
+
+	void add(std::string_view task)
+	{
+		m_fileDirty = true;
+		m_tasks.push_back(task);
+	}
+
+	void remove(int id);
+
+	const Task* getTaskById(const int id) const 
+	{
+		auto found = std::find_if(m_tasks.begin(), m_tasks.end(), [id](const Task& task)
+	       {
+	       return (task.getId() == id);
+	       });
+		if(found != m_tasks.end())
+		{
+			return &(*found);
+		}
+		return nullptr;
+	}
+
+	const std::vector<Task> getTasksByProject(std::string_view project) const
+	{
+		auto searcher = [project](const Task& task)
+			    {
+				auto task_projects {task.getProjects()};
+				auto found = std::find(task_projects.begin(), task_projects.end(), project);
+				return (found != task_projects.end());
+			    };
+		return getTasksBy(searcher);
+	}
+
+	const std::vector<Task> getTasksByTag(std::string_view tag) const
+	{
+		auto searcher = [tag](const Task& task)
+			    {
+				auto task_tags {task.getContexts()};
+				auto found = std::find(task_tags.begin(), task_tags.end(), tag);
+				return (found != task_tags.end());
+			    };
+		return getTasksBy(searcher);
+	}
+
+	const std::vector<Task> getTasksByCreationDate(std::string_view date) const;
+	const std::vector<Task> getTasksByCreationDate(const Date& date) const;
+	const std::vector<Task> getCompletedTasks() const
+	{
+		auto searcher = [](const Task& task)
+			    {
+				return task.isCompleted();
+			    };
+		return getTasksBy(searcher);
+	}
+	const std::vector<Task> getUnfinishedTasks() const
+	{
+		auto searcher = [](const Task& task)
+			    {
+				return !task.isCompleted();
+			    };
+		return getTasksBy(searcher);
+	}
+	// Adding by member wise
+	// void add(prio, things)
+	// {
+	// 	m_file_dirty = true;
+	// 	m_tasks.push_back(task);
+	// }
+
+};
 
 int main()
 {
@@ -333,7 +535,7 @@ int main()
 	std::vector<std::pair<int,int>> lines_pos{};
 
 	std::vector<Task> temp_file_tasks{};
-	
+
 	int i{};
 	for(Task curr_task{}; file>>curr_task;i++)
 	{
@@ -368,7 +570,7 @@ int main()
 		Task{"x 2026-01-04 2025-08-05 Make the best @todo program in the world in +C++ for +learning the language for @quitting from +TWI due:2026-01-01 Plan:A Type:learning"},
 		Task{" 2026-01-04 2025-08-05 Make the best @todo program in the world in +C++ for +learning the language for @quitting from +TWI due:2026-01-01 Plan:A Type:learning"},
 		Task{"2026-01-04 2025-08-05 Make the best @todo program in the world in +C++ for +learning the language for @quitting from +TWI due:2026-01-01 Plan:A Type:learning"},
-		
+
 		Task{"x (D) 2025-08-05 Make the best @todo program in the world in +C++ for +learning the language for @quitting from +TWI due:2026-01-01 Plan:A Type:learning"},
 		Task{"x  2025-08-05 Make the best @todo program in the world in +C++ for +learning the language for @quitting from +TWI due:2026-01-01 Plan:A Type:learning"},
 		Task{"x 2025-08-05 Make the best @todo program in the world in +C++ for +learning the language for @quitting from +TWI due:2026-01-01 Plan:A Type:learning"},
@@ -385,7 +587,7 @@ int main()
 
 	};
 
-	
+
 
 	// std::sort()
 	std::sort(std::execution::seq, tasks.begin(), tasks.end(), [](const Task& task1, const Task& task2)
@@ -394,7 +596,7 @@ int main()
 	   return task1 > task2;
 	   });
 
-	std::sort(std::execution::seq, tasks.begin(), tasks.end(), [](const Task& task1, const Task& task2)
+	std::sort(std::execution::seq, temp_file_tasks.begin(), temp_file_tasks.end(), [](const Task& task1, const Task& task2)
 	   {
 	   // return task1.m_isCompleted > task2.m_isCompleted;
 	   return task1 > task2;
